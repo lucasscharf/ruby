6cdf3981d6329d8af3350250869474a760051279
Aleatório <lucas.scharf@gmail.com>
2026-09-11 18:04:52 -0300

    add microposts

--- hello_app/config/routes.rb
+++ hello_app/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   get "up" => "rails/health#show", as: :rails_health_check
-  root 'application#ola'
+  root 'users#index'
 end
--- hello_app/db/schema.rb
+++ hello_app/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_11_201205) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_11_205855) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
